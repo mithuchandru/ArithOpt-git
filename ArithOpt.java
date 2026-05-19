5fc66b422bdfc49e692fbccf6b533c3ff5f4d508
mithuchandru <mithuchandru30@gmail.com>
2026-05-19 19:31:13 +0530

    Initial commit

---
+++ ArithOpt.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class ArithOpt
+{
+    public static void main(String[] args)
+    {
+        int x=5;
+        int y=3;
+        System.out.println("Addition:"+(x+y));
+        System.out.println("Subtraction:"+(x-y));
+
+    }
+}
+
+
