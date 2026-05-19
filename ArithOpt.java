e4369726f989338fa7046681207a8e2d2c3a1906
mithuchandru <mithuchandru30@gmail.com>
2026-05-19 19:45:32 +0530

    Version 2

--- ArithOpt.java
+++ ArithOpt.java
@@ -7,6 +7,8 @@ class ArithOpt
         int y=3;
         System.out.println("Addition:"+(x+y));
         System.out.println("Subtraction:"+(x-y));
+        System.out.println("Multiplication:"+(x*y));
+        System.out.println("Division:"+(x/y));
 
     }
 }
